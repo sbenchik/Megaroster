750acfd4c3f8cf8bc5f7fd7b785c9dfe6f78b6d9
Steve Benchik <stevebenchik@gmail.com>
2017-05-16 14:01:54 -0400

    Append submitted names to the bottom of the page

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const App = {
+    
     init(){
         const nameForm = document.querySelector('form')
         nameForm.addEventListener('submit', (ev) => this.onSubmit(ev))
+    },
+
+    createList(person){
+        const nameList = document.createElement('ul')
+        Array.from(person).map((input) => {
+            if(input.value){
+                let name = input.value
+                let li = this.createListItem(name)
+                nameList.appendChild(li)
+            }
+        })
+        return nameList
+    },
+
+    createListItem(name){
+        const newName = document.createElement('li')
+        newName.textContent = name
+        return newName
+    },
+
+    onSubmit(ev){
+        ev.preventDefault()
+        const form = ev.target
+        const names = document.querySelector('.names')
+        const list = this.createList(form.elements)
+        names.appendChild(list)
     },
 }
 App.init()
