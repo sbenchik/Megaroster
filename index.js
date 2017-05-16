e24db294461cc3febc48ec31fdf851f49b16f017
Steve Benchik <stevebenchik@gmail.com>
2017-05-16 15:17:31 -0400

    Add delete button next to entries

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const App = {
         nameForm.addEventListener('submit', (ev) => this.onSubmit(ev))
     },
 
+    removeItem(arr, toRemove){
+        arr.splice(arr.findIndex( (target)=>{
+            return target === toRemove
+        }), 1)
+    },
+
+    onClick(ev){
+        ev.preventDefault()
+        const listItem = ev.target.parentNode
+        listItem.parentNode.removeChild(listItem)
+    },
+
     createList(person){
         const nameList = document.querySelector('ul')
         Array.from(person).map((input) => {
@@ -21,6 +33,12 @@ const App = {
     createListItem(name){
         const newName = document.createElement('li')
         newName.textContent = name
+
+        const deleteButton = document.createElement('button')
+        deleteButton.textContent = 'Delete'
+        deleteButton.addEventListener('click', (ev) => this.onClick(ev))
+
+        newName.appendChild(deleteButton)
         return newName
     },
 
